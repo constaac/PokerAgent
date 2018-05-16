a57e58188db3c75ad3b0df428a15c1730969ba70
constaac <aconstantine719@gmail.com>
2018-05-16 15:37:50 -0400

    Basic Setup of Realm CRUD Services & Error Handling via Alerts
    Implements:
    Create, Update, Read functions
    Post function to post errors to notification center
    Start/Stop Observing for error notification functions
    
    closes #2

--- PokerAgent/Services/RealmService.swift
+++ PokerAgent/Services/RealmService.swift
@@ -7,3 +7,63 @@
 //
 
 import Foundation
+import RealmSwift
+
+class RealmService {
+    
+    private init() {}
+    static let shared = RealmService()
+    var realm = try! Realm()
+    
+    
+    func create<T: Object>(_ object: T) {
+        do {
+            try realm.write {
+                realm.add(object)
+            }
+        } catch {
+            post(error)
+        }
+    }
+    
+    func update<T: Object>(_ object: T, with dictionary: [String: Any?]) {
+        do {
+            try realm.write {
+                for (key, value) in dictionary {
+                    object.setValue(value, forKey: key)
+                }
+            }
+        } catch {
+            post(error)
+        }
+    }
+    
+    func delete<T: Object>(_ object: T) {
+        do {
+            try realm.write {
+                realm.delete(object)
+            }
+        } catch {
+            post(error)
+        }
+    }
+    
+    
+    // Posts Error to Notification Center
+    func post(_ error: Error) {
+        NotificationCenter.default.post(name: NSNotification.Name("RealmError"), object: error)
+    }
+    
+    // Adds Observer to Current VC to Listen for RealmErrors
+    func observeRealmErrors(in vc: UIViewController, completion: @escaping (Error?) -> Void) {
+        NotificationCenter.default.addObserver(forName: NSNotification.Name("RealmError"), object: nil, queue: nil) { (notification) in
+            completion(notification.object as? Error)
+        }
+    }
+    
+    // Removes Observer from Current VC
+    func stopObservingRealmErrors(in vc: UIViewController) {
+        NotificationCenter.default.removeObserver(vc, name: NSNotification.Name("RealmError"), object: nil)
+    }
+    
+}
