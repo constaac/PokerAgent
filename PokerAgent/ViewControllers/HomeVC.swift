b4e24da240654a5b962e76b35ac8d78f66fc13b5
constaac <aconstantine719@gmail.com>
2018-05-16 15:09:50 -0400

    Creates Moch File Structure and Renames HomeVC

--- PokerAgent/ViewControllers/HomeVC.swift
+++ PokerAgent/ViewControllers/HomeVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeVC.swift
 //  PokerAgent
 //
 //  Created by Alex Constantine on 5/16/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
