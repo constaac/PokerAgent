6c3d33d567d911595a912950872ac4bca113aed7
constaac <aconstantine719@gmail.com>
2018-05-16 15:29:23 -0400

    Initializes RealmService and Player/Session Model Files

---
+++ PokerAgent/Models/Player.swift
@@ -0,0 +1,9 @@
+//
+//  Player.swift
+//  PokerAgent
+//
+//  Created by Alex Constantine on 5/16/18.
+//  Copyright © 2018 Alex Constantine. All rights reserved.
+//
+
+import Foundation
---
+++ PokerAgent/Services/RealmService.swift
@@ -0,0 +1,9 @@
+//
+//  RealmService.swift
+//  PokerAgent
+//
+//  Created by Alex Constantine on 5/16/18.
+//  Copyright © 2018 Alex Constantine. All rights reserved.
+//
+
+import Foundation
