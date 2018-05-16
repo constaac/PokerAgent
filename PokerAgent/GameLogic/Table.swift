c17822aa0c00b547884b0ee278b3f4cdb94dc2f6
constaac <aconstantine719@gmail.com>
2018-05-16 19:32:08 -0400

    Prepares for GameBoard Configuration
    Scaffolds 4 action functions in GameLogic Class
    References shared realm service
    
    Creates ActiveSessionVC Class and storyboard object
    
    References #5

---
+++ PokerAgent/GameLogic/Table.swift
@@ -0,0 +1,32 @@
+//
+//  Table.swift
+//  PokerAgent
+//
+//  Created by Alex Constantine on 5/16/18.
+//  Copyright © 2018 Alex Constantine. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class GameLogic {
+    
+    let realm = RealmService.shared.realm
+//    var currentPlayerSessions: Results<PlayerSession.self>!
+    
+    func betAction() {
+        
+    }
+    
+    func callAction() {
+        
+    }
+    
+    func checkAction() {
+        
+    }
+    
+    func foldAction() {
+        
+    }
+}
