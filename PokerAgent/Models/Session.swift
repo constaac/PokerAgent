b3b62ceb1aa34e741343e53337ba1aae4811331b
constaac <aconstantine719@gmail.com>
2018-05-16 15:46:28 -0400

    Configures basic Session Entity

--- PokerAgent/Models/Session.swift
+++ PokerAgent/Models/Session.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+import RealmSwift
+
+@objcMembers class Session: Object {
+    dynamic var id = UUID().uuidString
+    dynamic var startDate: Date = Date()
+    dynamic var endDate: Date? = nil
+    dynamic var location: String? = ""
+    dynamic var handsObserved: Int16 = 0
+    dynamic var chipPosition: Int8 = 0
+    //    dynamic var players: [Player]
+    
+    convenience init(location: String) {
+        self.init()
+        self.location = location
+    }
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
