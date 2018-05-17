df41571a996909e0c81c69b86bba220512dfaf2e
constaac <aconstantine719@gmail.com>
2018-05-17 16:00:26 -0400

    Removes PlayerSession Entity in favor of Direct Many-Many
    Removes PlayerSession Entity
    
    Links Session and Player via List<Respective Entity>
    
    TODO: Implement LinkedOjects
    
    References #5

--- PokerAgent/Models/Player.swift
+++ PokerAgent/Models/Player.swift
@@ -10,17 +10,29 @@ import Foundation
 import RealmSwift
 
 @objcMembers class Player: Object {
-    dynamic var id = UUID().uuidString
+    dynamic var id = 0
     dynamic var name: String = "Anon"
+    dynamic var isUser: Bool = false
     dynamic var isAnon: Bool = true
-    dynamic var comments: String? = nil
+    dynamic var comments: String? = ""
     dynamic var discoveryDate: Date = Date()
-    dynamic var playerSessions: [String] = []
+    dynamic var sessions: List<Session> = List<Session>()
+    dynamic var currentSession: Session? = nil
     
-    convenience init(sessionID: String, comments: String?) {
+    convenience init(sessionID: String, isUser: Bool?, comments: String?) {
         self.init()
+        if let isUser = isUser {
+            self.isUser = isUser
+        }
         self.comments = comments
-        self.playerSessions.append(sessionID)
+    }
+    
+    func addSession() -> Void {
+        
+    }
+    
+    func removeSession() -> Void {
+        
     }
     
     override static func primaryKey() -> String? {
--- PokerAgent/Models/Session.swift
+++ PokerAgent/Models/Session.swift
@@ -10,18 +10,28 @@ import Foundation
 import RealmSwift
 
 @objcMembers class Session: Object {
-    dynamic var id = UUID().uuidString
+    dynamic var id = 0
     dynamic var startDate: Date = Date()
     dynamic var endDate: Date? = nil
     dynamic var location: String = ""
     dynamic var handsObserved: Int16 = 0
     dynamic var chipPosition: Int8 = 0
-    dynamic var playerSessions: [String] = []
+    dynamic var players: List<Player> = List<Player>()
     
-    convenience init(location: String, playerSessions: [String]) {
+    convenience init(location: String) {
         self.init()
         self.location = location
-        self.playerSessions = playerSessions
+    }
+    
+    func addPlayer(player: Player) -> Void {
+        players.append(player)
+    }
+    
+    func removePlayer(player: Player) -> Void {
+        let playerIndex = players.index(of: player)
+        if let playerIndex = playerIndex {
+            players.remove(at: playerIndex)
+        }
     }
     
     override static func primaryKey() -> String? {
