d052a783e1e95324b4c405868f4e62b5dca65e3c
constaac <aconstantine719@gmail.com>
2018-05-21 12:50:53 -0400

    Adds Detatchable Object Service to Create Objects Detatched from the Realm File

---
+++ PokerAgent/Services/DetachObjectService.swift
@@ -0,0 +1,46 @@
+//
+//  DetachableObjectService.swift
+//  PokerAgent
+//
+//  Created by Alex Constantine on 5/19/18.
+//  Copyright © 2018 Alex Constantine. All rights reserved.
+//
+
+import Realm
+import RealmSwift
+
+protocol DetachableObject: AnyObject {
+    func detached() -> Self
+}
+
+extension Object: DetachableObject {
+    func detached() -> Self {
+        let detached = type(of: self).init()
+        for property in objectSchema.properties {
+            guard let value = value(forKey: property.name) else {
+                continue
+            }
+            if let detachable = value as? DetachableObject {
+                detached.setValue(detachable.detached(), forKey: property.name)
+            } else {
+                detached.setValue(value, forKey: property.name)
+            }
+        }
+        return detached
+    }
+}
+
+extension List: DetachableObject {
+    func detached() -> List<Element> {
+        let result = List<Element>()
+        forEach {
+            if let detachableObject = $0 as? DetachableObject,
+                let element = detachableObject.detached() as? Element {
+                result.append(element)
+            } else {
+                result.append($0)
+            }
+        }
+        return result
+    }
+}
