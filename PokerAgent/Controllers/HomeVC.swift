376270c6efacd55e8f5bf1d4913ec67c4b9a8e75
constaac <aconstantine719@gmail.com>
2018-05-21 12:50:16 -0400

    Renames ViewControllers file to COntrollers and Adds Age Range Enumeration

---
+++ PokerAgent/Controllers/HomeVC.swift
@@ -0,0 +1,25 @@
+//
+//  HomeVC.swift
+//  PokerAgent
+//
+//  Created by Alex Constantine on 5/16/18.
+//  Copyright © 2018 Alex Constantine. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
